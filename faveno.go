2a77c0deedc0c27d229ed9a26de423616950787f
rezamakbar <rezamakbar13@gmail.com>
2021-08-07 00:24:23 +0700

    learndua

---
+++ faveno.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	angka := 100
+// 	fmt.Print("Nomor Favorit saya adalah: ", rand.Intn(angka))
+// }
+
+// func main() {
+// 	var i, j int = 1, 2
+// 	var c, python bool
+// 	c = true
+// 	python = false
+// 	var java string = "nol"
+
+// 	fmt.Println(i, j, c, python, java)
+// }
+
+// function akar di parsing ke float, karena sqrt returnnya float
+// func main(){
+// 	x := float64(3)
+// 	y := float64(4)
+// 	z := math.Sqrt(x)
+// 	u := math.Sqrt(y)
+// 	fmt.Println(z, u)
+
+//  }
+
+func main ()  {
+	i := 42
+	f := 3.142
+	g := 0.867 + 0.51
+
+	fmt.Printf("i Mempunyai tipe data:  %T\n", i)
+	fmt.Printf("f Mempunyai tipe data:  %g\n", f)
+	fmt.Printf("g Mempunyai tipe data:  %T\n", g)
+}
+
+// func main () {
+// i, j := 42, 2701
+// po := &i
+// fmt.Println(*po)
+// *po = 21
+// fmt.Println(po)
+// po = &j
+// j = (*po / 37)
+// fmt.Println(j)
+
+// }
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("hello world")
+
+	// var name string
+	// name = "Reza"
+
+	// fmt.Println("Reza" + name)
+
+	// var age int 
+	// age = 20
+
+	// fmt.Println(age)
+	// // var isPredicate bool
+	// // isPredicate = false
+
+	// fmt.Println("umur saya", age)
+
+	// // // fmt.Println("Nama Saya: " + name + "umur saya" +age)
+
+	// // // count := 20 //shorthand variabel
+
+	// artist := "blackpink"
+
+	// if artist == "Blackpink" {
+	// 	fmt.Println("Korea")
+	// } else {
+	// 	fmt.Println("Not Korea")
+	// }
+
+	// switch artist {
+	// case "Blackpink":
+	// 	fmt.Println("Korea")
+	// 	break
+	// }
+
+	// for i := 0; i < 10; i++{
+	// 	fmt.Println(i)
+	// }
+
+	// // array := ["Apple", "Manggo"]
+	// // for x, v := range array{
+	// // 	fmt.Printf()
+
+	// // } 
+
+	// sum := 1
+	// for sum < 20 {
+	// 	sum += sum
+	// }
+	// fmt.Println(sum)
+
+	number := 20
+
+	pointerNumb := &number // alokasi data ke dalam virtual memori
+	fmt.Println(*pointerNumb) // cara akses valuenya *+variabel
+
+
+
+
+}
