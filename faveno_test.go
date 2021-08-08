1f88cb5692048644a4bc924698b2c6797c57d635
rezamakbar <rezamakbar13@gmail.com>
2021-08-08 12:00:32 +0700

    unit test

--- faveno_test.go
+++ faveno_test.go
@@ -14,3 +14,25 @@ func TestLuasPersegi(t *testing.T)  {
 		t.Errorf("Salah, seharusnya : %v", luasExpected)
 	}
 }
+
+var (
+	persegipanjang PersegiPanjang = PersegiPanjang{panjang: 2}
+	persegipanjang1 PersegiPanjang = PersegiPanjang{panjang: 4}
+	hasilExpected float64 = 
+
+)
+
+func TestPenjumlahan(t *testing.T)  {
+	t.Logf("Penjumlahannya adalah :  %v", tambah())
+
+	if tambah() != luasExpected {
+		t.Errorf("Salah, seharusnya : %v", jmlExpected)
+	}
+
+	
+}
+
+
+func (pp PersegiPanjang) Luas() float64  {
+	return 2 * (pp.Panjang * pp.Lebar)
+}
